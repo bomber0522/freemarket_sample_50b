6868e600a86d1444ebc37ab143669ec8b1a99446
Abe Yukari <sakura@AbenoMacBook-Air.local>
2019-08-17 14:47:24 +0900

    ユーザーのサーバーサイドの実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,24 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_product, only: [:new, :pay, :address, :card, :card_index]
+
+  require 'payjp'
+  require 'date'
+
   def new
+    @card = current_user.card
+    if @card
+      Payjp.api_key = Rails.application.credentials.payjp[:test_secret_key]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
     render layout: 'application-off-header-footer.html.haml'
   end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
+
 end
